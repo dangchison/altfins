70a74173afb6740be7e43622f3ffd7475433d7b2
dangchison <dangchison@gmail.com>
2026-04-09 18:21:06 +0700

    hidden browser

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 def setup_driver():
   options = webdriver.ChromeOptions()
-  # options.add_argument("--headless=new")
+  options.add_argument("--headless=new")
   options.add_argument("--disable-gpu")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
